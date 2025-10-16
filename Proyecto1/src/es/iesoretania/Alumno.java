737c02d07bd498ca892551bd08c67474c93d47be
llv <llv@llv-VirtualBox>
2025-10-16 20:06:49 +0200

    version 1.1

---
+++ Proyecto1/src/es/iesoretania/Alumno.java
@@ -0,0 +1,33 @@
+package es.iesoretania;
+
+public class Alumno {
+
+	private String nombre;
+	private String apellidos;
+	
+	public Alumno(String nombre, String apellidos) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	
+	
+	
+	
+}
--- Proyecto1/src/es/iesoretania/Hola.java
+++ Proyecto1/src/es/iesoretania/Hola.java
@@ -6,6 +6,7 @@ public class Hola {
 
 
 		System.out.println("Hola mundo !!!!!!!");
+		System.out.println("Adios mundo !!!!!!!");
 	}
 
 }
